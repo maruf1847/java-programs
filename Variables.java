e8bc7e39c2adac605da5f15b519f90538602b34b
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2022-08-26 09:44:15 +0600

    variable files add

---
+++ Variables.java
@@ -0,0 +1,12 @@
+package com.basicjava;
+
+public class Variables {
+    public static void main(String[] args){
+        String name = "Arifur Rahman";
+        System.out.println(name);
+        int age = 30;
+        float number = 30.12f;
+        boolean myBool = true;
+        char myLetter = 'M';
+    }
+}
