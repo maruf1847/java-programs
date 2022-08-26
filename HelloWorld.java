b282c954a5857c337652e2171eb2eb1b456fa440
Marufur Rahman <59738629+maruf1847@users.noreply.github.com>
2022-08-26 09:34:46 +0600

    first program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package com.basicjava;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello Maruf");
+        System.out.println("Hello World");
+    }
+}
